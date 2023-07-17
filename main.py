4bd32f5e18ece46e8ceff1bc6396f396ba02e280
supersiem <100954549+supersiem@users.noreply.github.com>
2023-07-17 18:13:23 +0200

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-camra = 1
 def change_camra():
     global camra
     if camra == 1:
@@ -9,3 +8,91 @@ def change_camra():
         camra = 4
     elif camra == 4:
         camra = 1
+def move():
+    if controller.player1.is_pressed(ControllerButton.B):
+        change_camra()
+    if controller.player1.is_pressed(ControllerButton.RIGHT):
+        player_1.x += 2.5
+    if controller.player1.is_pressed(ControllerButton.LEFT):
+        player_1.x += -2.5
+    if controller.player1.is_pressed(ControllerButton.DOWN):
+        player_1.y += 2.5
+    if controller.player1.is_pressed(ControllerButton.UP):
+        player_1.y += -2.5
+    if controller.player2.is_pressed(ControllerButton.RIGHT):
+        player_2.x += 2.5
+    if controller.player2.is_pressed(ControllerButton.LEFT):
+        player_2.x += -2.5
+    if controller.player2.is_pressed(ControllerButton.DOWN):
+        player_2.y += 2.5
+    if controller.player2.is_pressed(ControllerButton.UP):
+        player_2.y += -2.5
+    if controller.player4.is_pressed(ControllerButton.RIGHT):
+        player_camra.x += 2.5
+    if controller.player4.is_pressed(ControllerButton.LEFT):
+        player_camra.x += -2.5
+    if controller.player4.is_pressed(ControllerButton.DOWN):
+        player_camra.y += 2.5
+    if controller.player4.is_pressed(ControllerButton.UP):
+        player_camra.y += -2.5
+    if controller.player3.is_pressed(ControllerButton.UP):
+        player_3.y += -2.5
+    if controller.player3.is_pressed(ControllerButton.RIGHT):
+        player_3.x += 2.5
+    if controller.player3.is_pressed(ControllerButton.LEFT):
+        player_3.x += -2.5
+    if controller.player3.is_pressed(ControllerButton.DOWN):
+        player_3.y += 2.5
+    if controller.player3.is_pressed(ControllerButton.UP):
+        player_3.y += -2.5
+def update_camra():
+    if camra == 1:
+        scene.camera_follow_sprite(player_1)
+    elif camra == 2:
+        scene.camera_follow_sprite(player_2)
+    elif camra == 3:
+        scene.camera_follow_sprite(player_3)
+    elif camra == 4:
+        scene.camera_follow_sprite(player_camra)
+camra = 0
+player_camra: Sprite = None
+player_3: Sprite = None
+player_2: Sprite = None
+player_1: Sprite = None
+player_1 = sprites.create(assets.image("""
+    myImage
+"""), SpriteKind.player)
+player_2 = sprites.create(assets.image("""
+    myImage0
+"""), SpriteKind.player)
+player_3 = sprites.create(assets.image("""
+    myImage1
+"""), SpriteKind.player)
+player_camra = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.player)
+camra = 1
+tiles.set_current_tilemap(tilemap("""
+    level1
+"""))
+scene.camera_follow_sprite(player_1)
+
+def on_on_update():
+    move()
+game.on_update(on_on_update)
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-let camra = 1
-function change_camra() {
-    
+controller.player4.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    player_camra = sprites.create(assets.image`myImage2`, SpriteKind.Player)
+})
+function change_camra () {
     if (camra == 1) {
         camra = 2
     } else if (camra == 2) {
@@ -10,6 +11,129 @@ function change_camra() {
     } else if (camra == 4) {
         camra = 1
     }
-    
 }
-
+controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    player_camra = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
+controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    player_1.sayText(camra, 1000, false)
+    change_camra()
+    player_1.sayText(camra, 1000, false)
+})
+function move () {
+    if (controller.player1.isPressed(ControllerButton.B)) {
+        change_camra()
+    }
+    if (controller.player1.isPressed(ControllerButton.Right)) {
+        player_1.x += 2.5
+    }
+    if (controller.player1.isPressed(ControllerButton.Left)) {
+        player_1.x += -2.5
+    }
+    if (controller.player1.isPressed(ControllerButton.Down)) {
+        player_1.y += 2.5
+    }
+    if (controller.player1.isPressed(ControllerButton.Up)) {
+        player_1.y += -2.5
+    }
+    if (controller.player2.isPressed(ControllerButton.Right)) {
+        player_2.x += 2.5
+    }
+    if (controller.player2.isPressed(ControllerButton.Left)) {
+        player_2.x += -2.5
+    }
+    if (controller.player2.isPressed(ControllerButton.Down)) {
+        player_2.y += 2.5
+    }
+    if (controller.player2.isPressed(ControllerButton.Up)) {
+        player_2.y += -2.5
+    }
+    if (controller.player4.isPressed(ControllerButton.Right)) {
+        player_camra.x += 2.5
+    }
+    if (controller.player4.isPressed(ControllerButton.Left)) {
+        player_camra.x += -2.5
+    }
+    if (controller.player4.isPressed(ControllerButton.Down)) {
+        player_camra.y += 2.5
+    }
+    if (controller.player4.isPressed(ControllerButton.Up)) {
+        player_camra.y += -2.5
+    }
+    if (controller.player3.isPressed(ControllerButton.Up)) {
+        player_3.y += -2.5
+    }
+    if (controller.player3.isPressed(ControllerButton.Right)) {
+        player_3.x += 2.5
+    }
+    if (controller.player3.isPressed(ControllerButton.Left)) {
+        player_3.x += -2.5
+    }
+    if (controller.player3.isPressed(ControllerButton.Down)) {
+        player_3.y += 2.5
+    }
+    if (controller.player3.isPressed(ControllerButton.Up)) {
+        player_3.y += -2.5
+    }
+}
+function update_camra () {
+    if (camra == 1) {
+        scene.cameraFollowSprite(player_1)
+    } else if (camra == 2) {
+        scene.cameraFollowSprite(player_2)
+    } else if (camra == 3) {
+        scene.cameraFollowSprite(player_3)
+    } else if (camra == 4) {
+        scene.cameraFollowSprite(player_camra)
+    }
+}
+let camra = 0
+let player_camra: Sprite = null
+let player_3: Sprite = null
+let player_2: Sprite = null
+let player_1: Sprite = null
+player_1 = sprites.create(assets.image`myImage`, SpriteKind.Player)
+player_2 = sprites.create(assets.image`myImage0`, SpriteKind.Player)
+player_3 = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+player_camra = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+camra = 1
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(player_1)
+game.onUpdate(function () {
+    move()
+    update_camra()
+})
