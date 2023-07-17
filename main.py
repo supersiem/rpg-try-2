cc3dfd4d52057d13194a45a693a387844f5dfa4c
supersiem <100954549+supersiem@users.noreply.github.com>
2023-07-17 17:36:20 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+camra = 1
+def change_camra():
+    global camra
+    if camra == 1:
+        camra = 2
+    elif camra == 2:
+        camra = 3
+    elif camra == 3:
+        camra = 4
+    elif camra == 4:
+        camra = 1
---
+++ main.ts
@@ -0,0 +1,15 @@
+let camra = 1
+function change_camra() {
+    
+    if (camra == 1) {
+        camra = 2
+    } else if (camra == 2) {
+        camra = 3
+    } else if (camra == 3) {
+        camra = 4
+    } else if (camra == 4) {
+        camra = 1
+    }
+    
+}
+
